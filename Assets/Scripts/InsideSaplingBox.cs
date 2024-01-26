7e8b1d7c74ef93eff6960d1f8b4d2637e2401584
KatiaGOAT <147525378+KatiaGOAT@users.noreply.github.com>
2024-01-26 12:25:46 +0000

    pushing final project

--- Assets/Scripts/InsideSaplingBox.cs
+++ Assets/Scripts/InsideSaplingBox.cs
@@ -1,49 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class InsideTreasureBox : MonoBehaviour
+public class InsideSaplingBox : MonoBehaviour
 {
     //this script is to detect whether the solar panel entered its respective box
     private AudioSource TreasureAudio;
 
-    public GameObject SolarPanel;
-    public Image SolarPanelBg;
-    public bool SolarPanelCollected=false;
+    public GameObject Sapling;
+    public bool SaplingCollected = false;
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
         GetComponent<TriggerCollection>().OnEnterEvent.AddListener(InsideCollector);
         //call this function when on trigger function is called
 
         // This returns the GameObject named SolarPanel.
-        SolarPanel = GameObject.Find("SolarPanel");
-        Debug.Log("Solar Panel found");
-
-        //SolarPanelBg = GameObject.Find("SolarimageBg").GetComponent<Image>(); //taking a ref of the objects image component to access it color property
-        //Debug.Log("Solar Panel BG found");
-        //Debug.Log(SolarPanelBg.tintColor.ToString());
+        Sapling = GameObject.Find("Sapling");
+        Debug.Log("Sapling found");
 
         TreasureAudio = GetComponent<AudioSource>();//to get the audio source on this component
     }
-
     public void InsideCollector(GameObject Obj)
     {
         //to check if the object going the box is the same as the picture above
-        //we has this script for 1 particular script, so we will compare with that set obj only, here with solarpanel
+        //we has this script for 1 particular script, so we will compare with that set obj only, here with sapling
 
-        if (GameObject.ReferenceEquals(Obj, SolarPanel))
+        if (GameObject.ReferenceEquals(Obj, Sapling))
         {
             Debug.Log("first and second are the same");
             Obj.SetActive(false);   //making the object disappear when it is in the box
             TreasureAudio.Play(); //to play the audio on collision
-            SolarPanelCollected=true;
-            Debug.Log(SolarPanelCollected);
+            SaplingCollected = true;
+            Debug.Log(SaplingCollected);
         }
         else
             Debug.Log("objects dont match");
-
     }
 
     // Update is called once per frame
